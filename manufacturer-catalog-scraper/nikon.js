33ed345d59615bea3f0e9c2b96854145c9b7d1c5
Matt La Rue <mlarue@keh.com>
2021-06-23 17:05:23 -0400

    Initial commit for Nikon scraper

---
+++ manufacturer-catalog-scraper/nikon.js
@@ -0,0 +1,14 @@
+/* 
+    Load in common functions and variables
+*/
+const COMMON = require('./common');
+const puppeteer = COMMON.puppeteer
+const cheerio = COMMON.cheerio
+const chalk = COMMON.chalk
+const jspdf = COMMON.jspdf
+const fs =  COMMON.fs
+const html2canvas = COMMON.html2canvas
+const jsdom = COMMON.jsdom
+const { JSDOM } = jsdom;
+const axios = COMMON.axios
+const SCRAPINGBEE = COMMON.SCRAPINGBEE
